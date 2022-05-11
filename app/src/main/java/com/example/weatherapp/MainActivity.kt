4ac5e3f7e8541f1c341c7823cb621ad9d16c0327
中川真人 <masanew01@gmail.com>
2022-05-11 15:12:49 +0900

    修正

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,27 +1,46 @@
 package com.example.weatherapp
 
+
+import android.Manifest
+import android.content.ContentValues.TAG
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.weatherapp.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        locationinformationpermission()
+        locationinformation()
 
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO) {
+                tastBackground()
+            }
+        }
     }
 
     override fun onRequestPermissionsResult(
+
+
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
@@ -38,15 +57,25 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun locationinformationpermission() {
+
+    private fun locationinformation() {
+        //パーミッションの状態確認
 
         //位置情報許可されている
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            //位置情報許可されていない
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "位置情報許可されています")
+        //位置情報許可されていない
         } else {
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION), 100)
+            Log.d(TAG, "位置情報許可されていないのでバーミッションダイアログを表示しました。")
+            //パーミッションダイアログ表示
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 100)
         }
     }
 
+    private fun tastBackground() {
+        //緯度経度取
 
+        Log.d(TAG , "通過")
+
+    }
 }
